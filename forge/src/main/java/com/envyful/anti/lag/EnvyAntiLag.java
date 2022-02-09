5d0cf47951f41753bd86fb07c967cec83d06cc66
Daniel Norris <dnorris443@gmail.com>
2022-02-09 18:06:16 +0000

    feat(forge): add main forge class

---
+++ forge/src/main/java/com/envyful/anti/lag/EnvyAntiLag.java
@@ -0,0 +1,37 @@
+package com.envyful.anti.lag;
+
+import com.envyful.api.forge.command.ForgeCommandFactory;
+import com.envyful.api.forge.player.ForgePlayerManager;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(
+        modid = EnvyAntiLag.MOD_ID,
+        version = EnvyAntiLag.MOD_VERSION,
+        name = EnvyAntiLag.MOD_NAME,
+        acceptableRemoteVersions = "*"
+)
+public class EnvyAntiLag {
+
+    public static final String MOD_ID = "envyantilag";
+    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_NAME = "EnvyAntiLag";
+
+    @Mod.Instance(MOD_ID)
+    private static EnvyAntiLag instance;
+
+    private ForgePlayerManager playerManager = new ForgePlayerManager();
+    private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
+
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+
+}
